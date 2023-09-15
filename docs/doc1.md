034d08160d54d3764cd42a3d90619f72c22084d9
Renato <renatommiguel@gmail.com>
2023-09-15 23:12:41 +0200

    wip

---
+++ docs/doc1.md
@@ -0,0 +1,10 @@
+# Notes
+
+### 15-09-2023
+
+- Robot is space separated 2-4 spaces or tabs
+- Pipe can also be used '|'
+- Test should use natural language so it is easier to undertand -> reason behind the keywords.
+- Builtin are installed with robot framework 
+- https://robotframework.org/
+- 
--- src/Libraries/MathLibrary.py
+++ src/Libraries/MathLibrary.py
@@ -2,19 +2,20 @@
 Test idea from chat gpt
 """
 
+
 class MathLibrary:
     ROBOT_TEST_SCOPE = 'SUITE'
 
-    def add(self, a:int, b:int):
+    def add(self, a: int, b: int):
         return a + b
 
-    def subtract(self, a:int, b:int):
+    def subtract(self, a: int, b: int):
         return a - b
 
-    def multiply(self, a:int, b:int):
+    def multiply(self, a: int, b: int):
         return a * b
 
-    def divide(self, a:int, b:int):
+    def divide(self, a: int, b: int):
         if b == 0:
             raise ValueError("Division by zero is not allowed.")
         return a / b
